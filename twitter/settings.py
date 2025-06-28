36b676f5bb0785a100ab9a39bee4685354cbb09d
Mohammad Sajjad Nouri <mhmdsajadno@gmail.com>
2025-06-28 19:14:55 +0330

    api is surprisingly working

--- twitter/settings.py
+++ twitter/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'twitterApp'
+    'rest_framework',
+    'twitterApp',
+    'api',
 ]
 
 MIDDLEWARE = [
